d18868b2554d862608a6797b5092ec7cca31a21c
rusEFI LLC <rusefillc@gmail.com>
2024-10-31 10:18:59 -0400

    MassUpdater to deal with manual DFU only:mre_f4

--- java_console/ui/src/main/java/MassUpdater.java
+++ java_console/ui/src/main/java/MassUpdater.java
@@ -1,5 +1,8 @@
+import com.rusefi.NamedThreadFactory;
 import com.rusefi.SerialPortScanner;
+import com.rusefi.core.rusEFIVersion;
 import com.rusefi.maintenance.jobs.AsyncJobExecutor;
+import com.rusefi.maintenance.jobs.DfuManualJob;
 import com.rusefi.maintenance.jobs.OpenBltManualJob;
 import com.rusefi.ui.StatusWindow;
 import com.rusefi.ui.widgets.ToolButtons;
@@ -10,6 +13,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 import static com.rusefi.SerialPortScanner.SerialPortType.OpenBlt;
@@ -19,9 +23,17 @@ public class MassUpdater {
     private final Set<String> knownBlts = new HashSet<>();
 
     public MassUpdater() {
-        mainStatus.showFrame("Mass Updater");
+        mainStatus.showFrame("Mass Updater " + rusEFIVersion.CONSOLE_VERSION);
+
+        final AtomicBoolean previousDfuState = new AtomicBoolean();
 
         SerialPortScanner.INSTANCE.addListener(currentHardware -> {
+
+            if (!previousDfuState.get() && currentHardware.isDfuFound()) {
+                SwingUtilities.invokeLater(() -> AsyncJobExecutor.INSTANCE.executeJob(new DfuManualJob()));
+            }
+            previousDfuState.set(currentHardware.isDfuFound());
+
             List<SerialPortScanner.PortResult> currentBltList = currentHardware.getKnownPorts().stream().filter(portResult -> portResult.type == OpenBlt).collect(Collectors.toList());
             Set<String> currentSet = currentBltList.stream().map(portResult -> portResult.port).collect(Collectors.toSet());
             for (Iterator<String> it = knownBlts.iterator(); it.hasNext(); ) {
