7a05e53e9770999e98ecb4f90094cef4d6ca0122
vlad <63706438+Vd2617@users.noreply.github.com>
2025-02-14 10:02:31 +0300

    111111111

--- firmware/controllers/can/can_verbose.cpp
+++ firmware/controllers/can/can_verbose.cpp
@@ -42,7 +42,7 @@ void reloadErrors(){
   lastErrorCodeIndex = i - 1;
 }
 
-uint16_t getNextErrorCode() {
+void uint16_t getNextErrorCode() {
   if (lastErrorCodeIndex < 0) {
       reloadErrors();
   }
