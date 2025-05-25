b7f6950aa64d2c8979083da883d18354ad3ade5e
vlad <63706438+Vd2617@users.noreply.github.com>
2025-05-25 14:30:28 +0300

    TEST_FOR_NEW_Update trigger_chrysler.cpp

--- firmware/controllers/trigger/decoders/trigger_chrysler.cpp
+++ firmware/controllers/trigger/decoders/trigger_chrysler.cpp
@@ -528,37 +528,33 @@ void initJeep_XJ_4cyl_2500(TriggerWaveform *s) {
 }
 
 void configureChryslerNGC_36_2_2(TriggerWaveform *s) {
-	s->initialize(FOUR_STROKE_CAM_SENSOR, SyncEdge::RiseOnly);
+s->initialize(FOUR_STROKE_CRANK_SENSOR, SyncEdge::RiseOnly);
 
-	float wide = 30 * 2;
-	float narrow = 10 * 2;
+    float narrow = 6;      //  6°
+    float toothWidth = 4;  //  4°
+    float wide = 24;       // long teeth 24°
 
-	s->setTriggerSynchronizationGap(3.5);
-	for (int i = 1; i < 15; i++) {
-		s->setTriggerSynchronizationGap3(/*gapIndex*/i, TRIGGER_GAP_DEVIATION_LOW, TRIGGER_GAP_DEVIATION_HIGH);
-	}
-	s->setTriggerSynchronizationGap3(/*gapIndex*/15, 0.4 * TRIGGER_GAP_DEVIATION_LOW, 0.4 * TRIGGER_GAP_DEVIATION_HIGH);
+    //(gap = 26°)
+    s->setTriggerSynchronizationGap3(0, 4.0, 5.0);
 
-	float base = 0;
+    float base = 0;
 
-	for (int i = 0; i < 14; i++) {
-		s->addEventAngle(base + narrow / 2, TriggerValue::RISE, TriggerWheel::T_PRIMARY);
-		s->addEventAngle(base + narrow, TriggerValue::FALL, TriggerWheel::T_PRIMARY);
-		base += narrow;
-	}
+    // 15 
+    for (int i = 0; i < 15; i++) {
+        s->addEventAngle(base, TriggerValue::RISE, TriggerWheel::T_PRIMARY);
+        s->addEventAngle(base + toothWidth, TriggerValue::FALL, TriggerWheel::T_PRIMARY);
+        base += narrow;
+    }
 
-	s->addEventAngle(base + narrow / 2, TriggerValue::RISE, TriggerWheel::T_PRIMARY);
-	base += narrow / 2;
-	s->addEventAngle(base + wide, TriggerValue::FALL, TriggerWheel::T_PRIMARY);
-	base += wide;
+    // long teeth
+    s->addEventAngle(base, TriggerValue::RISE, TriggerWheel::T_PRIMARY);
+    s->addEventAngle(base + wide, TriggerValue::FALL, TriggerWheel::T_PRIMARY);
+    base += wide;
 
-	for (int i = 0; i < 16; i++) {
-		s->addEventAngle(base + narrow / 2, TriggerValue::RISE, TriggerWheel::T_PRIMARY);
-		s->addEventAngle(base + narrow, TriggerValue::FALL, TriggerWheel::T_PRIMARY);
-		base += narrow;
-	}
-
-	// one small tooth at the end of the engine cycle
-	s->addEventAngle(s->getCycleDuration() - narrow / 2, TriggerValue::RISE, TriggerWheel::T_PRIMARY);
-	s->addEventAngle(s->getCycleDuration(), TriggerValue::FALL, TriggerWheel::T_PRIMARY);
+    // 16 
+    for (int i = 0; i < 16; i++) {
+        s->addEventAngle(base, TriggerValue::RISE, TriggerWheel::T_PRIMARY);
+        s->addEventAngle(base + toothWidth, TriggerValue::FALL, TriggerWheel::T_PRIMARY);
+        base += narrow;
+    }
 }
